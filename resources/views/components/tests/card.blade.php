67dd82cefc7ad59c6475eea961474112bbed513b
yama-choki <lovepingpong0@gmail.com>
2022-11-08 21:23:37 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,10 @@
+@props([
+  //初期値を設定するのであれば全ての値に初期値の記載をしなければならない
+  'title',//初期値に何も入れない場合
+  'message' => 'messageの初期値です。',
+  'content' => 'contentの初期値です',
+])
+
 <div class="border-2 shadow-md w-1/4 p2">
   <div>{{ $title }}</div>
   <div>画像</div>
