ebbdd401d540b6840ba5a3d2042f8f5c6bfeb751
yama-choki <lovepingpong0@gmail.com>
2022-11-08 21:31:08 +0900

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -5,7 +5,9 @@
   'content' => 'contentの初期値です',
 ])
 
-<div class="border-2 shadow-md w-1/4 p2">
+<div {{ $attributes->merge([
+  'class' => 'border-2 shadow-md w-1/4 p2'
+  ])}}>
   <div>{{ $title }}</div>
   <div>画像</div>
   <div>{{ $content }}</div>
