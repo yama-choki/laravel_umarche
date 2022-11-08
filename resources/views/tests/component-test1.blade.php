3ab50861881cb72d348ffdc53f60dd7c59c9ff22
yama-choki <lovepingpong0@gmail.com>
2022-11-08 21:15:12 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,5 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー１</x-slot>
   コンポーネントテスト１
-  <x-tests.card title="タイトル" content="本文" />
+  <x-tests.card title="タイトル" content="本文" :message="$message"/>
 </x-tests.app>
