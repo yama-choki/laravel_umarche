38133b04372fc3db3d837d6dc1a94613816ed0de
yama-choki <lovepingpong0@gmail.com>
2022-11-10 22:41:23 +0900

    クラスベース

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,7 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー２</x-slot>
   コンポーネントテスト2
+  <x-test-class-base classBaseMessage="メッセージですあああああ"/>
+  <div class="mb-6"></div>
+  <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更してます"/>
 </x-tests.app>
